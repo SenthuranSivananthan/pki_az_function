bb95c5c74ed726ab990d3039000900ea2c34da0b
Senthuran Sivananthan <sesivan@microsoft.com>
2021-01-05 09:51:28 -0500

    Encrypt & Decrypt files in Blob Storage using PKI

---
+++ DecryptFiles.cs
@@ -0,0 +1,112 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Keys.Cryptography;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class DecryptFiles
+    {
+        // credit: https://www.c-sharpcorner.com/article/encryption-and-decryption-using-a-symmetric-key-in-c-sharp/
+        public static string DecryptString(byte[] key, byte[] cipherText)  
+        {  
+            byte[] iv = new byte[16];  
+            byte[] buffer = cipherText;
+  
+            using (Aes aes = Aes.Create())  
+            {  
+                aes.Key = key;
+                aes.IV = iv;  
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);  
+  
+                using (MemoryStream memoryStream = new MemoryStream(buffer))  
+                {  
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))  
+                    {  
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))  
+                        {  
+                            return streamReader.ReadToEnd();  
+                        }  
+                    }  
+                }  
+            }  
+        }
+        
+        [FunctionName("DecryptFiles")]
+        public static async Task Run(
+            [BlobTrigger("encrypted/{name}.json", Connection = "sesivanpki_STORAGE")]TextReader inputBlob,
+            [Blob("decrypted/{name}", FileAccess.Write, Connection = "sesivanpki_STORAGE")]Stream outputBlob,
+            string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name}.json");
+
+            var envelope = JsonConvert.DeserializeObject<EncryptionEnvelope>(inputBlob.ReadToEnd());
+
+            // Decrypt the random key
+            var credentials = new AzureCliCredential();
+            var keyClient = new KeyClient(new Uri("https://sesivanpki.vault.azure.net"), credentials);
+            var privateKey = await keyClient.GetKeyAsync("pkiprivate");
+
+            var cryptoClient = new CryptographyClient(privateKey.Value.Id, credentials);
+            var decryptedRandomKey = cryptoClient.Decrypt(EncryptionAlgorithm.RsaOaep, Convert.FromBase64String(envelope.Key));
+
+            // Use random key to decrypt data
+            var decryptedData = DecryptString(decryptedRandomKey.Plaintext, Convert.FromBase64String(envelope.Data));
+            var decryptedBase64Data = Convert.FromBase64String(decryptedData);
+            var decryptedOriginalData = Encoding.UTF8.GetString(decryptedBase64Data, 0, decryptedBase64Data.Length);
+
+            var decryptedOriginalDataBytes = Encoding.UTF8.GetBytes(decryptedOriginalData);
+            outputBlob.Write(decryptedOriginalDataBytes, 0, decryptedOriginalDataBytes.Length);
+
+            log.LogInformation($"Decryption complete for {name}.json");
+        }
+
+        public static byte[] RandomKey(int length)
+        {
+            var rng = RandomNumberGenerator.Create();
+            byte[] key = new byte[length];
+            rng.GetBytes(key);
+
+            return key;
+        }
+
+        // credit: https://www.c-sharpcorner.com/article/encryption-and-decryption-using-a-symmetric-key-in-c-sharp/
+        public static string EncryptString(byte[] key, string plainText)  
+        {  
+            byte[] iv = new byte[16];  
+            byte[] array;  
+
+            using (Aes aes = Aes.Create())  
+            {  
+                aes.Key = key;
+                aes.IV = iv;  
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);  
+
+                using (MemoryStream memoryStream = new MemoryStream())  
+                {  
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))  
+                    {  
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))  
+                        {  
+                            streamWriter.Write(plainText);  
+                        }  
+
+                        array = memoryStream.ToArray();  
+                    }  
+                }  
+            }  
+
+            return Convert.ToBase64String(array);  
+        }
+    }
+}
---
+++ EncryptFiles.cs
@@ -0,0 +1,105 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public class EncryptionEnvelope
+    {
+        public EncryptionEnvelope() {}
+        
+        public string Key { get; set; }
+        public string Data { get; set; }
+        public string FileName { get; set; }
+    }
+
+    public static class EncryptFiles
+    {
+        [FunctionName("EncryptFiles")]
+        public static async Task Run(
+            [BlobTrigger("raw/{name}", Connection = "sesivanpki_STORAGE")]Stream inputBlob,
+            [Blob("encrypted/{name}.json", FileAccess.Write, Connection = "sesivanpki_STORAGE")]Stream outputBlob,
+            string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {inputBlob.Length} Bytes");
+
+            var envelope = new EncryptionEnvelope();
+            envelope.FileName = name;
+
+            var credentials = new AzureCliCredential();
+
+            // generate random key
+            var randomKey = RandomKey(32);
+
+            // encrypt random key with public key (asymmetric key)
+            var secretClient = new SecretClient(new Uri("https://sesivanpki.vault.azure.net"), credentials);
+            var secret = await secretClient.GetSecretAsync("pkipublic");
+            var x509 = new X509Certificate2(Convert.FromBase64String(secret.Value.Value));
+            var randomKeyEncrypted = x509.GetRSAPublicKey().Encrypt(randomKey, RSAEncryptionPadding.OaepSHA1);
+
+            // save random key as base64 encoded string
+            envelope.Key = Convert.ToBase64String(randomKeyEncrypted);
+
+            // encode data to base64
+            var data = new byte[inputBlob.Length];
+            inputBlob.Read(data, 0, (int)inputBlob.Length);
+            var dataAsBase64 = Convert.ToBase64String(data);
+
+            // encrypt data using random key (symmetric key)
+            envelope.Data = EncryptString(randomKey, dataAsBase64);
+
+            // save to output
+            var serializedOutput = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(envelope));
+            outputBlob.Write(serializedOutput, 0, serializedOutput.Length);
+
+            log.LogInformation($"Encryption complete for {name}");
+        }
+
+        public static byte[] RandomKey(int length)
+        {
+            var rng = RandomNumberGenerator.Create();
+            byte[] key = new byte[length];
+            rng.GetBytes(key);
+
+            return key;
+        }
+
+        // credit: https://www.c-sharpcorner.com/article/encryption-and-decryption-using-a-symmetric-key-in-c-sharp/
+        public static string EncryptString(byte[] key, string plainText)  
+        {  
+            byte[] iv = new byte[16];  
+            byte[] array;  
+
+            using (Aes aes = Aes.Create())  
+            {  
+                aes.Key = key;
+                aes.IV = iv;  
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);  
+
+                using (MemoryStream memoryStream = new MemoryStream())  
+                {  
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))  
+                    {  
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))  
+                        {  
+                            streamWriter.Write(plainText);  
+                        }  
+
+                        array = memoryStream.ToArray();  
+                    }  
+                }  
+            }  
+
+            return Convert.ToBase64String(array);  
+        }
+    }
+}
